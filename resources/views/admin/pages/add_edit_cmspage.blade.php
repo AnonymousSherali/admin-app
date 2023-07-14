461a382c682813c66cf7a630fb76c7d78d4acacd
AnonymousSherali <anonimsherali@gmail.com>
2023-07-14 18:11:10 +0500

    Update add_edit_cmspage.blade.php

--- resources/views/admin/pages/add_edit_cmspage.blade.php
+++ resources/views/admin/pages/add_edit_cmspage.blade.php
@@ -43,33 +43,20 @@
                                     @csrf
                                     <div class="card-body">
                                         <div class="form-group">
-                                            <label for="title">Title</label>
+                                            <label for="title">Title*</label>
                                             <input type="text" class="form-control" id="title" name="title"
-                                                placeholder="Enter Title">
+                                                placeholder="Enter Page Title">
                                         </div>
                                         <div class="form-group">
-                                            <label for="exampleInputPassword1">Password</label>
-                                            <input type="password" class="form-control" id="exampleInputPassword1"
-                                                placeholder="Password">
+                                            <label for="url">URL*</label>
+                                            <input type="text" class="form-control" id="url" name="url"
+                                                placeholder="Enter Page URL">
                                         </div>
                                         <div class="form-group">
-                                            <label for="exampleInputFile">File input</label>
-                                            <div class="input-group">
-                                                <div class="custom-file">
-                                                    <input type="file" class="custom-file-input" id="exampleInputFile">
-                                                    <label class="custom-file-label" for="exampleInputFile">Choose
-                                                        file</label>
-                                                </div>
-                                                <div class="input-group-append">
-                                                    <span class="input-group-text">Upload</span>
-                                                </div>
-                                            </div>
+                                            <label for="description">Description*</label>
+                                            <textarea name="description" id="description" rows="3" placeholder="Enter ..."></textarea>
                                         </div>
-                                        <div class="form-check">
-                                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                                        </div>
-                                        <div>
+                                        <div class="form-group">
                                             <button type="submit" class="btn btn-primary">Submit</button>
                                         </div>
                                     </div>
